5b776d8c6ea2596073b39dd3c26b4d5c9d3d1e72
Tajamul <tajamulali86@gmail.com>
2023-07-15 12:08:38 +0530

    correct sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,15 +1,43 @@
 export default function sitemap() {
     return [
       {
-        url: 'https://acme.com',
+        url: 'https://sweetwatersupplyriyadh.com',
         lastModified: new Date(),
       },
       {
-        url: 'https://acme.com/about',
+        url: 'https://sweetwatersupplyriyadh.com/about',
         lastModified: new Date(),
       },
       {
-        url: 'https://acme.com/blog',
+        url: 'https://sweetwatersupplyriyadh.com/service',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/contact',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/ar',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/ar/about',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/ar/service',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/ar/contact',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/privacy-policy',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://sweetwatersupplyriyadh.com/terms-and-conditions',
         lastModified: new Date(),
       },
     ]
