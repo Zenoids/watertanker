e717dd9835f280498688a715a1d127a33898c3f4
Tajamul <tajamulali86@gmail.com>
2023-07-15 11:51:43 +0530

    added good titles

--- app/(en)/contact/page.js
+++ app/(en)/contact/page.js
@@ -1,8 +1,8 @@
 import ContactForm from "../../component/contactform";
 
 export const metadata = {
-    title: 'Contact Sweet Water Supply - Get in Touch for Reliable Water Solutions ',
-    description: 'Contact Sweet Water Supply for all your water-related inquiries and requirements. Our dedicated team is ready to assist you with reliable and efficient water solutions tailored to your specific needs.',
+    title: 'Contact SWS Riyadh - Get in Touch for Reliable Water Solutions ',
+    description: 'Contact Sweet Water Supply riyadh for all your water-related inquiries and requirements. Our dedicated team is ready to assist you with reliable and efficient water solutions tailored to your specific needs.',
 }
 
 export default function Contact(){
--- app/(en)/service/page.js
+++ app/(en)/service/page.js
@@ -4,7 +4,7 @@ import Features from '@/app/component/feature'
 import TwoService from '@/app/component/twoservice'
 import React from 'react'
 export const metadata = {
-    title: 'Water Services by Sweet Water Supply - Trusted Solutions for Residential and Commercial Needs',
+    title: 'Services by Sweet Water Supply Riyadh - Trusted Solutions for Residential and Commercial Needs',
     description: 'Explore our comprehensive water services tailored to meet the diverse needs of residential and commercial clients. We provide uninterrupted access to high-quality water through our reliable delivery and maintenance solutions.',
 }
 export default function Service() {
