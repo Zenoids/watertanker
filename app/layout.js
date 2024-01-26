4db92bf12fe909ba059d7252e7be23ba36955951
Zenoids <web@zenoids.com>
2024-01-26 14:50:05 +0530

    aa

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return ( <html lang="en">
     <head> 
     <link rel="icon" href="favicon.ico"  />
+    <link rel="manifest" href="manifest.json" />
 
     </head>
       <body className={inter.className} >
